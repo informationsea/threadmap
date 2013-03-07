38e965b05cbf0647d55c96b76c677b5631e6994c
Y.Okamura <okamura@informationsea.info>
2013-03-07 23:15:08 +0900

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(name='threadmap',
-      version='0.1',
+      version='0.1.1',
       py_modules=['threadmap'],
       author='OKAMURA Yasunobu',
       author_email='okamura@informationsea.info',
